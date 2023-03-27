8e0957a7b9b7e112c6e45088fe9ac2db0d5f70ce
abraham-yusuf <1703020028@students.unis.ac.id>
2023-03-27 19:14:59 +0000

    update root home navbar

--- frontend/components/navigation/navbar.jsx
+++ frontend/components/navigation/navbar.jsx
@@ -3,7 +3,7 @@ import styles from "../../styles/Navbar.module.css";
 export default function Navbar() {
 	return (
 		<nav className={styles.navbar}>
-			<a href="https://alchemy.com/?a=create-web3-dapp" target={"_blank"}>
+			<a href="/">
 				<img className={styles.alchemy_logo} src="/cw3d-logo.png"></img>
 			</a>
 			<ConnectButton></ConnectButton>
